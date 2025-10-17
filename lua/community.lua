256c0f94d99c3d0ebbad0b38614ac0f259e29a48
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2025-10-17 14:24:26 +0200

    update plugins

--- lua/community.lua
+++ lua/community.lua
@@ -7,9 +7,12 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
+  -- { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
+  { import = "astrocommunity.editing-support.vim-visual-multi" },
   { import = "astrocommunity.motion.marks-nvim" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.motion.mini-move" }
+  { import = "astrocommunity.syntax.vim-easy-align" },
+  { import = "astrocommunity.motion.mini-move" },
+  -- { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
 }
